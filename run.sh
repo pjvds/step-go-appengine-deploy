ac8fdab3c033a2ab8be5a654a910a43499a5ad79
Pieter Joost van de Sande <pj@born2code.net>
2013-08-15 15:30:14 +0200

    Adds functional logging

--- run.sh
+++ run.sh
@@ -2,8 +2,18 @@ sudo apt-get update
 sudo apt-get install unzip
 
 cd $WERCKER_ROOT
+debug 'Downloading AppEngine SDK...'
+
 curl -O http://googleappengine.googlecode.com/files/go_appengine_sdk_linux_amd64-1.8.3.zip ; unzip -x go_appengine_sdk_linux_amd64-1.8.3
 export PATH="$PATH:$(pwd)/go_appengine"
+
 echo "$WERCKER_APPENGINE_DEPLOY_PASSWORD" > "$WERCKER_STEP_TEMP/password"
+
+debug "using password: $(cat $WERCKER_STEP_TEMP/password)"
+
 cd $WERCKER_SOURCE_DIR
+
+debug 'Starting deployment'
 appcfg.py update ./ --email="$WERCKER_APPENGINE-DEPLOY_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
+
+success 'Finished'
