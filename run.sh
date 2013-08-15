34e2edc0d14106080939a03c76baf0bfa6ec1479
Pieter Joost van de Sande <pj@born2code.net>
2013-08-15 15:19:39 +0200

    Fix password option usage

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ sudo apt-get install unzip
 cd $WERCKER_ROOT
 curl -O http://googleappengine.googlecode.com/files/go_appengine_sdk_linux_amd64-1.8.3.zip ; unzip -x go_appengine_sdk_linux_amd64-1.8.3
 export PATH="$PATH:$(pwd)/go_appengine"
-echo "$WERCKER_APPENGINE-DEPLOY_PASSWORD" > "$WERCKER_STEP_TEMP/password"
+echo "$WERCKER_APPENGINE_DEPLOY_PASSWORD" > "$WERCKER_STEP_TEMP/password"
 cd $WERCKER_SOURCE_DIR
 appcfg.py update ./ --email="$WERCKER_APPENGINE-DEPLOY_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
