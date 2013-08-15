2e7c992b6a2117a0e49cf251c2d8da0cc4b11663
Pieter Joost van de Sande <pj@born2code.net>
2013-08-15 15:08:15 +0200

    Switch to go_appengine_sdk_linux_amd64-1.8.3

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install unzip
 
 cd $WERCKER_ROOT
-curl -O http://googleappengine.googlecode.com/files/go_appengine_sdk_darwin_amd64-1.8.3.zip ; unzip -x go_appengine_sdk_darwin_amd64-1.8.3
+curl -O http://googleappengine.googlecode.com/files/go_appengine_sdk_linux_amd64-1.8.3.zip ; unzip -x go_appengine_sdk_linux_amd64-1.8.3
 export PATH="$PATH:$(pwd)/go_appengine"
 echo "$WERCKER_APPENGINE-DEPLOY_PASSWORD" > "$WERCKER_STEP_TEMP/password"
 cd $WERCKER_SOURCE_DIR
