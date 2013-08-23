c00cadf18dcbf03b371a0bfb6aff00b133e80794
pjvds <pj@born2code.net>
2013-08-23 16:52:31 +0200

    Rename option vars according to step rename

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@ debug 'Downloading AppEngine SDK...'
 curl -O http://googleappengine.googlecode.com/files/go_appengine_sdk_linux_amd64-1.8.3.zip ; unzip -x go_appengine_sdk_linux_amd64-1.8.3
 export PATH="$PATH:$(pwd)/go_appengine"
 
-echo "$WERCKER_APPENGINE_DEPLOY_PASSWORD" > "$WERCKER_STEP_TEMP/password"
+echo "$WERCKER_GO_APPENGINE_DEPLOY_PASSWORD" > "$WERCKER_STEP_TEMP/password"
 
 cd $WERCKER_SOURCE_DIR
 
 debug 'Starting deployment'
-appcfg.py update ./ --email="$WERCKER_APPENGINE_DEPLOY_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
+appcfg.py update ./ --email="$WERCKER_GO_APPENGINE_DEPLOY_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
 
 success 'Finished'
